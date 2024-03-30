a69f0471cc45379e51fc77da66a8477e4c032ca6
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2024-03-30 16:38:56 -0300

    feat: Add users dto

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -6,18 +6,22 @@ import {
   Param,
   Patch,
   Post,
+  Put,
 } from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
+  ApiInternalServerErrorResponse,
   ApiNoContentResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
+  ApiParam,
   ApiTags,
 } from '@nestjs/swagger';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { UpdateUserDto } from '../dto/update-user.dto';
+import { UUIDParamDto } from '../dto/uuid-param.dto.input';
 import { UsersService } from '../services/users.service';
 
 @Controller('users')
@@ -26,43 +30,79 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  @ApiOperation({ summary: 'Create a user' })
-  @ApiCreatedResponse({ description: 'Created response' })
-  @ApiBadRequestResponse({ description: 'Bad Request response' })
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @ApiOperation({ summary: 'Criar um usuário' })
+  @ApiCreatedResponse({ description: 'Usuário criado' })
+  @ApiBadRequestResponse({ description: 'Solicitação incorreta' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
   }
 
   @Get()
-  @ApiOperation({ summary: 'List of all users' })
-  @ApiOkResponse({ description: 'OK response' })
+  @ApiOperation({ summary: 'Listar todos os usuários' })
+  @ApiOkResponse({ description: 'Consulta realizada com sucesso' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
   findAll() {
     return this.usersService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Find a user' })
-  @ApiOkResponse({ description: 'OK response' })
-  @ApiBadRequestResponse({ description: 'Bad Request response' })
-  @ApiNotFoundResponse({ description: 'Not Found response' })
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @ApiOperation({ summary: 'Buscar um usuário' })
+  @ApiParam({
+    description: 'User id',
+    name: 'id',
+    example: '986dcaf4-c1ea-4218-b6b4-e4fd95a3c28e',
+  })
+  @ApiOkResponse({ description: 'Consulta realizada com sucesso' })
+  @ApiBadRequestResponse({ description: 'Solicitação incorreta' })
+  @ApiNotFoundResponse({ description: 'Recurso não encontrado' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
+  findOne(@Param() { id }: UUIDParamDto) {
+    return this.usersService.findOne(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Atualizar um usuário' })
+  @ApiParam({
+    description: 'User id',
+    name: 'id',
+    example: '986dcaf4-c1ea-4218-b6b4-e4fd95a3c28e',
+  })
+  @ApiOkResponse({ description: 'Usuário atualizado' })
+  @ApiBadRequestResponse({ description: 'Solicitação incorreta' })
+  @ApiNotFoundResponse({ description: 'Recurso não encontrado' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
+  update(@Param() { id }: UUIDParamDto, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(id, dto);
   }
 
   @Patch(':id')
-  @ApiOperation({ summary: 'Update a user' })
-  @ApiOkResponse({ description: 'OK response' })
-  @ApiNotFoundResponse({ description: 'Not Found response' })
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  @ApiOperation({ summary: 'Atualizar parcialmente um usuário' })
+  @ApiParam({
+    description: 'User id',
+    name: 'id',
+    example: '986dcaf4-c1ea-4218-b6b4-e4fd95a3c28e',
+  })
+  @ApiOkResponse({ description: 'Usuário atualizado parcialmente' })
+  @ApiBadRequestResponse({ description: 'Solicitação incorreta' })
+  @ApiNotFoundResponse({ description: 'Recurso não encontrado' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
+  updatePartial(@Param() { id }: UUIDParamDto, @Body() dto: UpdateUserDto) {
+    return this.usersService.update(id, dto);
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Remove a user' })
-  @ApiNoContentResponse({ description: 'No Content response' })
-  @ApiBadRequestResponse({ description: 'Bad Request response' })
-  @ApiNotFoundResponse({ description: 'Not Found response' })
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @ApiOperation({ summary: 'Remover um usuário' })
+  @ApiParam({
+    description: 'User id',
+    name: 'id',
+    example: '986dcaf4-c1ea-4218-b6b4-e4fd95a3c28e',
+  })
+  @ApiNoContentResponse({ description: 'Usuário removido' })
+  @ApiBadRequestResponse({ description: 'Solicitação incorreta' })
+  @ApiNotFoundResponse({ description: 'Recurso não encontrado' })
+  @ApiInternalServerErrorResponse({ description: 'Erro Interno do Servidor' })
+  remove(@Param() { id }: UUIDParamDto) {
+    return this.usersService.remove(id);
   }
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsAlpha, IsEmail, IsString, IsStrongPassword } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsAlpha()
+  @ApiProperty()
+  readonly name: string;
+
+  @IsEmail()
+  @ApiProperty()
+  readonly email: string;
+
+  @IsStrongPassword({
+    minLength: 6,
+    minNumbers: 0,
+    minLowercase: 0,
+    minUppercase: 0,
+    minSymbols: 0,
+  })
+  @ApiProperty({ minLength: 6 })
+  readonly password: string;
+}
