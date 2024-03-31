1e8593d7291520abb0663d0f322d4e709636682f
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2024-03-30 22:10:05 -0300

    feat: Add prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      Logger.log('Shutting down');
+      await this.$disconnect();
+      await app.close();
+    });
+  }
+}
